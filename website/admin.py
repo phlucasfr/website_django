eb94012af616f701402978d549f33b83d9139c96
Phelipe Lucas <phlucasfr@gmail.com>
2021-03-31 09:24:13 -0300

    Adcionando campo 'DELETED' ao ADMIN.

--- website/admin.py
+++ website/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'sub_title', 'full_name', 'categories']
+    list_display = ['title', 'sub_title', 'full_name', 'categories', 'deleted']
     search_fields = ['title', 'sub_title']
-    #fields = ['title', 'sub_title']
+
+    # fields = ['title', 'sub_title']
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=True)
 
 admin.site.register(Post, PostAdmin)
 
